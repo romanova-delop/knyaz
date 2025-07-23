7077a12a3ac3902c36eebd111971a4a018591454
romanova-delop <sany2006romanova@gmail.com>
2025-07-23 10:50:21 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,18 @@
-﻿
-const CACHE_NAME = 'my-pwa-cache-v1';
+﻿const CACHE_NAME = 'trebricon-cache-v1';
+const OFFLINE_URL = 'offline.html';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/styles/main.css',
-  '/scripts/main.js',
-  '/images/logo.png'
-  '/manifest.json'
+  '/styles.css',
+  '/offline.html',
+  '/fon1.jpg',
+  '/dom1.jpg',
+  '/ban1.jpg',
+  '/bas1.png',
+  '/sar1.jpg',
+  '/gar1.jpg',
+  '/tep1.jpeg'
 ];
-
 
 self.addEventListener('install', event => {
   event.waitUntil(
@@ -19,12 +23,34 @@ self.addEventListener('install', event => {
   );
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
 
 self.addEventListener('fetch', event => {
-  event.responWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch (event.request);
-
-     })
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .catch(() => {
+          return caches.match(OFFLINE_URL);
+        })
     );
-})
+  } else {
+    event.respondWith(
+      caches.match(event.request)
+        .then(response => {
+          return response || fetch(event.request);
+        })
+    );
+  }
+});
