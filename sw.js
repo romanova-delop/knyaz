357842b87768793168d49ac9eebbb65db3fcd243
romanova-delop <sany2006romanova@gmail.com>
2025-07-23 10:27:01 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,9 +6,10 @@ const urlsToCache = [
   '/styles/main.css',
   '/scripts/main.js',
   '/images/logo.png'
+  '/manifest.json'
 ];
 
-// Установка Service Worker
+
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -18,54 +19,12 @@ self.addEventListener('install', event => {
   );
 });
 
-// Активация Service Worker
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
 
-// Стратегия "Cache first, then network"
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Кэшированный ответ найден - возвращаем его
-        if (response) {
-          return response;
-        }
-        
-        // Делаем запрос к сети
-        return fetch(event.request).then(
-          response => {
-            // Проверяем, валидный ли ответ
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
+  event.responWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch (event.request);
 
-            // Клонируем ответ
-            const responseToCache = response.clone();
-
-            // Кэшируем новый ресурс
-            caches.open(CACHE_NAME)
-              .then(cache => {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        ).catch(() => {
-          // Можно вернуть fallback-страницу
-          return caches.match('/offline.html');
-        });
-      })
-  );
-});
+     })
+    );
+})
